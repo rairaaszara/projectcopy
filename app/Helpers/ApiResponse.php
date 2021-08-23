2423c77e51f3769d38f9a41f9578aac05956c61b
elhakimdev <abdulelhakim68@gmail.com>
2021-08-23 19:58:26 +0700

    fix api response docs

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -1,31 +1,48 @@
 <?php
 namespace App\Helpers;
 
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Error;
 
 class ApiResponse{
 
-    public static function success(string $message, mixed $data, $statusCode = 200)
+    /**
+     * The json success response
+     *
+     * @param string $message
+     * @param mixed $data
+     * @param integer $statusCode
+     * @return JsonResponse
+     */
+    public static function success(string $message, $data, $statusCode = 200): JsonResponse
+    {
+        $response = [
+            "success"   => true,
+            "message"   => $message,
+            "data"      => $data,
+        ];
+
+        return response()->json($response, $statusCode);
+    }
+
+    /**
+     * The errors response json.
+     *
+     * @param string $message
+     * @param mixed $data
+     * @param integer $statusCode
+     * @return JsonResponse
+     */
+    public static function error(string $message, $data, $statusCode = 400): JsonResponse
     {
 
-    $response = [
-        "success" => true,
-        "message" => $message,
-        "data" => $data,
-    ];
+        $response = [
+            "success"   => false,
+            "message"   => $message,
+            "data"      => $data,
+        ];
 
-    return response()->json($response);
-}
-
-    public static function error(string $message, mixed $data, $statusCode = 400)
-{
-
-    $response = [
-        "success" => false,
-        "message" => $message,
-        "data" => $data,
-    ];
-
-    return response()->json($response);
-}
- 
+        return response()->json($response, $statusCode);
+    }
 }
